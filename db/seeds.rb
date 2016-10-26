0ee002bdc53e60c61d056e3741676406834c16f9
hiroyuki <ikuyorihato@gmail.com>
2016-10-26 17:23:55 +0000

    fix seed1

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,33 +8,22 @@ end
 
 CSV.foreach('db/fixtures/product_utf8.csv') do |row|
   Product.create(
-      product_code: row[0],
-      name: row[1],
-      description: row[2],
-      price: row[3],
-      product_type: row[4],
-      jan_code: row[5])
+    product_code: row[0],
+    name: row[1],
+    description: row[2],
+    price: row[3],
+    product_type: row[4],
+    jan_code: row[5]
+    )
 end
 
 User.create(
-    name: "Hiroyuki Ota",
-    email: "ikuyorihato@gmail.com",
-    password: "hiroyuki21",
-    password_confirmation: "hiroyuki21",
-    admin: true
-    )
-User.create(
-    name: "Sample2",
-    email: "sample2@gmail.com",
-    password: "hiroyuki21",
-    password_confirmation: "hiroyuki21",
-    admin: false
-    )
-User.create(
-    name: "Sample3",
-    email: "sample3@gmail.com",
-    password: "hiroyuki21",
-    password_confirmation: "hiroyuki21",
-    admin: false
-    )
-    
+  name: "Hiroyuki Ota",
+  email: "ikuyorihato@gmail.com",
+  password: "hiroyuki21",
+  password_confirmation: "hiroyuki21",
+  payment_type: nil,
+  shipping_type: nil,
+  shipping_prefecture: nil,
+  admin: true
+  )
