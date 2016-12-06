af3948d35ebd6d1bfc51017f55e89ba5174c7323
hiroyuki <ikuyorihato@gmail.com>
2016-12-06 17:15:23 +0000

    analytics_init

--- Gemfile
+++ Gemfile
@@ -53,6 +53,8 @@ gem 'seed-fu'
 # セッションをデータベースで使う
 #gem 'activerecord-session_store'
 
+gem 'google-analytics-rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
+  # GoogleAnalytics
+  GA.tracker = "UA-88596833-1"
+  
   # Maile
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
